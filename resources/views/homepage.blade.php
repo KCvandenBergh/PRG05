14c76d33debf1e4de8ad13925e7dbd3b264535f4
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2022-10-06 15:19:10 +0200

    Alles even in Git geplaatst xx

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<title>Online Banking System</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+<header>Home</header>
+<header>Register</header>
+<article>
+    <h2>
+        Welcome to KC banking, how can we help you today?
+    </h2>
+
+    <p>
+        You can easily create a membership to KC banking. Click the 'I am new' button, or the <a href="register.blade.php"> button on the
+        right. If you are already a member click 'Log in'.
+    </p>
+</article>
+</body>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
